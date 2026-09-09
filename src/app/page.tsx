228a03310c19c7fee81dfff6fb15e606f4c28733
PiyushJ1 <piyxsh@gmail.com>
2026-09-09 16:47:06 +1000

    chore: switch section order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   return (
     <>
       <Component />
+      <ProductSection />
       <MissionSection />
       <ServicesSection />
-      <ProductSection />
     </>
   );
 }
