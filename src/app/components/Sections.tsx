cf1f6e41bfe7f01a498a66dfca8d5f311b00314c
PiyushJ1 <piyxsh@gmail.com>
2026-09-17 22:33:36 +1000

    chore: remove indentation

--- src/app/components/Sections.tsx
+++ src/app/components/Sections.tsx
@@ -251,7 +251,7 @@ export function ProductSection() {
             {productFeatures.map((feature) => (
               <li
                 key={feature}
-                className="flex items-start gap-3 text-md font-medium leading-relaxed"
+                className="flex items-start text-md font-medium leading-relaxed"
                 style={{ color: colors[300] }}
               >
                 <span
